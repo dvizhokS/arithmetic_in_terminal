e1d0a149e48831ba9a78729a35cbb3da51e25bbb
Sergei <seregashos@gmail.com>
2024-02-22 22:12:35 +0200

    added config file

---
+++ config.py
@@ -0,0 +1,23 @@
+"""
+    min_x - minimal first number;
+    max_x - maximal first number;
+    min_y - minimal second number;
+    max_y - maximal second number;
+    signs - signs for examples, possible signs( + - / * )
+        example:    "signs": "+",
+                    "signs": "+-*";
+    is_smarty - If you answer an example incorrectly, you need to answer two correctly, possible is_smarty("yes", "no")
+        example:    "is_smarty": True (mode enabled),
+                    "is_smarty": "False" (mode disabled);
+    example_to_win - number of examples that need to be solved to win
+"""
+
+config = {
+    "min_x": 0,
+    "max_x": 10,
+    "min_y": 0,
+    "max_y": 10,
+    "signs": "+-",
+    "is_smarty": True,
+    "example_to_win": 10,
+}
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from arithmetic.Arithmetic import Arithmetic
+from config import config
 
 
 if __name__ == "__main__":
     arithmetic = Arithmetic()
-    arithmetic.set_signs("+-")
+    arithmetic.set_min_x(config["min_x"])
+    arithmetic.set_max_x(config["max_x"])
+    arithmetic.set_min_y(config["min_y"])
+    arithmetic.set_max_y(config["max_y"])
+    arithmetic.set_signs(config["signs"])
+    arithmetic.set_smarty(config["is_smarty"])
+    arithmetic.set_example_to_win(config["example_to_win"])
     arithmetic.set_write_info_example(True)
+
     arithmetic.run()
 
 
